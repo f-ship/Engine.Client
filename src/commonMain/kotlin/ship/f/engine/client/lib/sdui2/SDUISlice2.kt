3e571bf0a703700961a9afb971f16b8ba3a44a02
Samuel Taiwo <samueltaiwo@stdev.co.uk>
2025-12-31 13:51:08 +0000

    [Implementation] Implemented safe error handling which graceful avoids crashes in almost every circumstances

--- src/commonMain/kotlin/ship/f/engine/client/lib/sdui2/SDUISlice2.kt
+++ src/commonMain/kotlin/ship/f/engine/client/lib/sdui2/SDUISlice2.kt
@@ -3,9 +3,12 @@ package ship.f.engine.client.lib.sdui2
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import ship.f.engine.client.utils.serverdrivenui3.ServerDrivenScreen3
+import ship.f.engine.client.utils.serverdrivenui3.ServerToastEvent
 import ship.f.engine.shared.core.Slice
+import ship.f.engine.shared.core.ToastEvent
 import ship.f.engine.shared.sdui2.SDUISubPub2
 import ship.f.engine.shared.sdui2.SDUISubPub2.SDUIState2
+import ship.f.engine.shared.utils.serverdrivenui2.ext.sduiLog
 
 object SDUISlice2 : Slice<SDUIState2, SDUISubPub2>(
     subPubClass = SDUISubPub2::class,
@@ -15,15 +18,23 @@ object SDUISlice2 : Slice<SDUIState2, SDUISubPub2>(
 
     @Composable
     override fun EntryPoint(state: MutableState<SDUIState2>) {
-//        ServerDrivenScreen2(
-//            projectName = state.value.projectName,
-//            resources = state.value.resources,
-//            vectors = state.value.vectors,
-//        )
+        sduiLog("sending ${state.value.toast}", tag = "EntryPoint > Toast") { state.value.toast != null}
         ServerDrivenScreen3(
             projectName = state.value.projectName,
             resources = state.value.resources,
             vectors = state.value.vectors,
+            toast = state.value.toast?.run {
+                ServerToastEvent(
+                    message = message,
+                    durationMs = durationMs,
+                    actionText = actionText,
+                    toastType = when(toastType){
+                        ToastEvent.ToastType.Error -> ServerToastEvent.ToastType.Error
+                        ToastEvent.ToastType.Success -> ServerToastEvent.ToastType.Success
+                        ToastEvent.ToastType.Warning -> ServerToastEvent.ToastType.Warning
+                    }
+                )
+            },
         )
     }
 
